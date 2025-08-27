ecb1e5374443db3bc702a63b95f88c7d426c9f0c
naoresponda-art <naoresponda@colegiosantoantonio.com.br>
2025-08-27 16:40:00 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,8 +1,16 @@
 # Leia uma linha com o número do cartão
-numero = input()
-
-# TODO: implemente a verificação pelo algoritmo de Luhn
-# Siga as dicas do README.
-
-# Ao final, imprima exatamente:
-# print("Cartão válido")  ou  print("Cartão inválido")
+numeros = input()
+impares= []
+for i in numeros[-1::-2]:
+  impares.append(int(i))
+pares = []
+for i in numeros[-2::-2]:
+  if 2*int(i)<10:
+    pares.append(2*int(i))
+  else:
+    pares.append(2*int(i)-10+1)
+soma = sum(pares)+sum(impares)
+if int(soma/10) == soma/10:
+  print("Cartão válido")
+else:
+  print("Cartão inválido")
